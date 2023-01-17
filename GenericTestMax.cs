f4856e2fa8ca9c2f646887d9391eb785626bc3ff
karankharole <karankharole2000@gmail.com>
2023-01-17 16:06:38 +0530

    refactor aal 3 UC and use iComparable

--- GenericTestMax.cs
+++ GenericTestMax.cs
@@ -6,25 +6,34 @@ using System.Threading.Tasks;
 
 namespace GenericsNew
 {
-    public class GenericsTestMax
+    public class GenericTestMax<T> where T : IComparable
     {
-        public static T MaxIntNumber<T>(T firstNum, T secondNum, T thirdNum) where T : IComparable
+        public T firstNum, secondNum, thirdNum;
+
+        public GenericTestMax(T firstNum, T secondNum, T thirdNum)
+        {
+            this.firstNum = firstNum;
+            this.secondNum = secondNum;
+            this.thirdNum = thirdNum;
+        }
+
+        public static T MaxIntegerNumber(T firstNum, T secondNum, T thirdNum)
         {
             if (firstNum.CompareTo(secondNum) > 0 && firstNum.CompareTo(thirdNum) > 0 || firstNum.CompareTo(secondNum) >= 0 && firstNum.CompareTo(thirdNum) > 0 || firstNum.CompareTo(secondNum) > 0 && firstNum.CompareTo(thirdNum) >= 0)
             {
                 return firstNum;
             }
-
             if (secondNum.CompareTo(firstNum) > 0 && secondNum.CompareTo(thirdNum) > 0 || secondNum.CompareTo(firstNum) >= 0 && secondNum.CompareTo(thirdNum) > 0 || secondNum.CompareTo(firstNum) > 0 && secondNum.CompareTo(thirdNum) >= 0)
             {
                 return secondNum;
             }
 
-            if (thirdNum.CompareTo(firstNum) > 0 && thirdNum.CompareTo(secondNum) > 0 || thirdNum.CompareTo(firstNum) >= 0 && thirdNum.CompareTo(secondNum) > 0 || thirdNum.CompareTo(firstNum) > 0 && thirdNum.CompareTo(secondNum) >= 0)
+            if (thirdNum.CompareTo(firstNum) > 0 && thirdNum.CompareTo(firstNum) > 0 || thirdNum.CompareTo(firstNum) >= 0 && thirdNum.CompareTo(firstNum) > 0 || thirdNum.CompareTo(firstNum) > 0 && thirdNum.CompareTo(firstNum) >= 0)
             {
                 return thirdNum;
             }
             return firstNum;
         }
+
     }
 }
--- Program.cs
+++ Program.cs
@@ -6,9 +6,9 @@
         {
 
 
-            Console.WriteLine("Maximum Interger Number is : " + GenericsTestMax.MaxIntNumber<int>(30, 20, 10));
-            Console.WriteLine("Maximum Float Number is : " + GenericsTestMax.MaxIntNumber<float>(30.3f, 45.20f, 76.10f));
-            Console.WriteLine("Maximum String Number is : " + GenericsTestMax.MaxIntNumber<string>("Peach", "Banana", "apple"));
+            Console.WriteLine("Maximum Interger Number is : " + GenericTestMax<int>.MaxIntegerNumber(30, 20, 10));
+            Console.WriteLine("Maximum Float Number is : " + GenericTestMax<float>.MaxIntegerNumber(30.3f, 45.20f, 76.10f));
+            Console.WriteLine("Maximum String Number is : " + GenericTestMax<string>.MaxIntegerNumber("AAA", "CCCCC", "HHHH"));
         }
     }
 }
