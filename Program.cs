2deff8b17e72022846695027e2247519090d3689
karankharole <karankharole2000@gmail.com>
2023-01-17 15:33:57 +0530

    max float value at 3rd position

--- Program.cs
+++ Program.cs
@@ -5,8 +5,8 @@
         static void Main(string[] args)
         {
 
-            double maxFlotingNo = GenericsMax.MaxIntNumber(85.4, 117.6, 19.0);
-            Console.WriteLine("\n Maximum floating number for 2nd position is = " + maxFlotingNo);
+            double maxFlotingNo = GenericsMax.MaxIntNumber(85.4, 117.6, 119.4);
+            Console.WriteLine("\n Maximum floating number for 3rd position is = " + maxFlotingNo);
         }
     }
 }
