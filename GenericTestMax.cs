ff8e09d98b4a96cfb0874edd253bc93d12ade2c3
karankharole <karankharole2000@gmail.com>
2023-01-17 08:25:02 +0530

    print max inr array

---
+++ GenericTestMax.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsNew
+{
+
+    public class GenericsMax
+    {
+        public static int MaxIntNumber(int firstNum, int secondNum, int thirdNum)
+        {
+            if (firstNum.CompareTo(secondNum) > 0 && firstNum.CompareTo(thirdNum) > 0 || firstNum.CompareTo(secondNum) >= 0 && firstNum.CompareTo(thirdNum) > 0 || firstNum.CompareTo(secondNum) > 0 && firstNum.CompareTo(thirdNum) >= 0)
+            {
+                return firstNum;
+            }
+
+            if (secondNum.CompareTo(firstNum) > 0 && secondNum.CompareTo(thirdNum) > 0 || secondNum.CompareTo(firstNum) >= 0 && secondNum.CompareTo(thirdNum) > 0 || secondNum.CompareTo(firstNum) > 0 && secondNum.CompareTo(thirdNum) >= 0)
+            {
+                return secondNum;
+            }
+
+            if (thirdNum.CompareTo(firstNum) > 0 && thirdNum.CompareTo(secondNum) > 0 || thirdNum.CompareTo(firstNum) >= 0 && thirdNum.CompareTo(secondNum) > 0 || thirdNum.CompareTo(firstNum) > 0 && thirdNum.CompareTo(secondNum) >= 0)
+            {
+                return thirdNum;
+            }
+            return firstNum;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace GenericsNew
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int maxNumber = GenericsMax.MaxIntNumber(854, 764, 909);
+            Console.WriteLine("\n Maximum Number is " + maxNumber);
+        }
+    }
+}
